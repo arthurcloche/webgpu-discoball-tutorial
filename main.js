26420e83d78b068eabd91de612d98a8413af4840
arthurcloche <arthur.cloche@shopify.com>
2025-06-16 17:06:41 -0400

    feat : mouse raytracer gpu

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 import * as THREE from "three/webgpu";
-import { positionLocal, vec4 } from "three/tsl";
+import {
+  positionLocal,
+  uv,
+  mul,
+  sub,
+  distance,
+  length,
+  vec2,
+  vec3,
+  vec4,
+  uniform,
+  smoothstep,
+  oneMinus,
+  instancedArray,
+  instanceIndex,
+  Fn,
+} from "three/tsl";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { DRACOLoader } from "three/examples/jsm/Addons.js";
 
-// Check for WebGPU support
+import cube from "./src/cube.glb?url";
+import ball from "./src/ball.glb?url";
+import env from "./src/env.jpg?url";
+
 if (!navigator.gpu) {
   throw new Error("WebGPU not supported");
 }
-let scene, camera, renderer, controls;
+
+let scene, camera, renderer, controls, mouse, computeUpdate, raycaster;
+const canvas = document.getElementById("webgpu");
 function setScene() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
@@ -15,16 +38,40 @@ function setScene() {
     0.1,
     1000
   );
-  camera.position.z = 5;
+  camera.position.z = 2;
 }
 
+// const textureLoader = new THREE.TextureLoader()
+
 function addObjects() {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicNodeMaterial({ color: 0x00ff00 });
-  material.positionNode = positionLocal;
-  material.colorNode = vec4(positionLocal.y, 0, 1, 1);
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const number = 100;
+  const mousePosition = uniform(mouse.position);
+  let particlePosition = new Float32Array(number * 3);
+  for (let i = 0; i < number; i++) {
+    particlePosition[i * 3 + 0] = Math.random() * 2 - 1;
+    particlePosition[i * 3 + 1] = Math.random() * 2 - 1;
+    particlePosition[i * 3 + 2] = Math.random() * 2 - 1;
+  }
+  const positions = instancedArray(particlePosition, "vec3");
+
+  computeUpdate = Fn(() => {
+    const position = positions.element(instanceIndex);
+    const distance = position.sub(mousePosition).length();
+    const sdf = smoothstep(0, 1, distance).oneMinus();
+    position.addAssign(vec3(sdf.mul(1), 0, 0));
+  })().compute(number);
+
+  const material = new THREE.SpriteMaterial({ color: 0x00ff00 });
+  // material.colorNode = Fn(() => {
+  //   return vec3(1, 1, 0);
+  // })();
+  material.colorNode = uv();
+  material.scaleNode = 0.2;
+  material.positionNode = positions.toAttribute();
+
+  const particles = new THREE.Sprite(material);
+  particles.count = number;
+  scene.add(particles);
 }
 
 function addControls() {
@@ -34,14 +81,54 @@ function addControls() {
   controls.enableZoom = false;
 }
 
+function addRaycaster() {
+  raycaster = new THREE.Raycaster();
+  raycaster.caster = new THREE.Mesh(
+    new THREE.PlaneGeometry(6, 3, 1, 1),
+    new THREE.MeshBasicNodeMaterial({
+      color: 0x00000,
+    })
+  );
+  scene.add(raycaster.caster);
+}
+
+function addMouse() {
+  mouse = new THREE.Vector2();
+  mouse.position = new THREE.Vector3(0, 0, 0);
+  mouse.update = (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    if (raycaster) {
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects([raycaster.caster]);
+      if (intersects.length > 0) {
+        // console.log(intersects[0].point);
+        mouse.position.copy(intersects[0].point);
+      }
+    }
+  };
+
+  window.addEventListener("mousemove", mouse.update);
+}
+
 const render = () => {
-  controls.update();
+  if (controls) controls.update();
   renderer.renderAsync(scene, camera);
+  renderer.compute(computeUpdate);
   requestAnimationFrame(render);
 };
 
+const resize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  window.addEventListener("resize", resize);
+};
+
 (async () => {
-  const canvas = document.getElementById("webgpu");
   renderer = new THREE.WebGPURenderer({
     canvas: canvas,
     antialias: true,
@@ -49,11 +136,16 @@ const render = () => {
   });
   await renderer.init().then(() => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    console.log("Renderer initialized");
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setClearColor("#0000AA");
+    console.log("Good to go!");
   });
 
   setScene();
-  addObjects();
   addControls();
+  addRaycaster();
+  addMouse();
+  addObjects();
+  resize();
   render();
 })();
